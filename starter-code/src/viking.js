e93a75da462e07a6ffa9ccc72ae4d1370e4634a7
Jorge Quintana <21299@columbushs.com>
2019-12-09 10:24:05 -0500

    first commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,37 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    recieveDamage(damage) {
+    
+        this.health = this.health - damage
+    }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        this.name = name
+        this.health = health
+        this.strength = strength
+    }
+    recieveDamage(damage) {
+        this.health = this.health - damage
+        if(this.health > 0) {
+            return `${name} has recieved ${damage} points of damage`
+        }
+        else {
+            return `${name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+}
 
 // Saxon
 class Saxon {}
